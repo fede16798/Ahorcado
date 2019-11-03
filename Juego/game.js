344906a42a003bb0d5f4ae5f104b5e2ee7952c09
fechee67@gmail.com <fechee67@gmail.com>
2019-11-03 18:59:41 -0300

    ordenando las carpetas

--- Juego/game.js
+++
@@ -1,5 +0,0 @@
-//Random word is selected and exported
-var wordsToGuess = ["pele"];
-var randoIndex = Math.floor(Math.random() * wordsToGuess.length);
-var randoWord = wordsToGuess[randoIndex];
-exports.randoWord = randoWord;
